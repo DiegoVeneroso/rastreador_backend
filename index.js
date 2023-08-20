8748125c38b297f03130079d6ae3aa988274bea2
Diego Veneroso <diegoveneroso.unipampa@gmail.com>
2023-08-19 22:04:35 -0300

    add date time

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 var gps = require("gps-tracking");
+let date_time = new Date();
+
+// get current date
+// adjust 0 before single digit date
+let date = ("0" + date_time.getDate()).slice(-2);
+
+// get current month
+let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+
+// get current year
+let year = date_time.getFullYear();
+
+// get current hours
+let hours = date_time.getHours();
+
+// get current minutes
+let minutes = date_time.getMinutes();
+
+// get current seconds
+let seconds = date_time.getSeconds();
 
 var options = {
     'debug'                 : false, //We don't want to debug info automatically. We are going to log everything manually so you can check what happens everywhere
@@ -44,7 +64,7 @@ var server = gps.server(options,function(device,connection){
     //Also, you can listen on the native connection object
     connection.on('data',function(data){
         //echo raw data package
-        console.log('data.toString'); 
+        console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
         console.log(data.toString()); 
     })
 

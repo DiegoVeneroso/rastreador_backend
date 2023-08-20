5c8176bf1531507ed1bd01eece4e64259d5ed09c
Diego Veneroso <diegoveneroso.unipampa@gmail.com>
2023-08-19 22:15:27 -0300

    ajuste hora

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var gps = require("gps-tracking");
-var date_ob = new Date();
+var moment = require('moment');
 
 
 var options = {
@@ -46,20 +46,8 @@ var server = gps.server(options,function(device,connection){
     //Also, you can listen on the native connection object
     connection.on('data',function(data){
 
-        var day = ("0" + date_ob.getDate()).slice(-2);
-var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-var year = date_ob.getFullYear();
-   
-var date = year + "-" + month + "-" + day;
-console.log(date);
-    
-var hours = date_ob.getHours();
-var minutes = date_ob.getMinutes();
-var seconds = date_ob.getSeconds();
-
-var dateTime = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
         //echo raw data package
-        console.log(dateTime);
+        console.log(moment().format('yyyy-mm-dd:hh:mm:ss'));
         console.log(data.toString()); 
     })
 
